7a400a416032cd0810ade55d591a42b6adf05632
Kyle <klim385@aucklanduni.ac.nz>
2024-03-23 22:15:58 +1300

    Implemented printVenues, correctly displays number of venues

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -1,5 +1,7 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
@@ -9,12 +11,21 @@ public class VenueHireSystem {
   private String venueCode;
   private String capacityInput;
   private String hireFeeInput;
+  private ArrayList<VenueHireSystem> venues = new ArrayList<>();
 
   public VenueHireSystem() {}
 
   public void printVenues() {
-    System.out.println(MessageCli.NO_VENUES);
-  }
+    int venueCount = venues.size();
+    if (venueCount == 0) {
+      System.out.println(MessageCli.NO_VENUES);
+    } countVenues(venueCount);
+
+    for (VenueHireSystem venue : venues) {
+      System.out.println(venue.venueName + " (" + venue.venueCode + ") - " + 
+      venue.capacityInput + " people - $" + venue.hireFeeInput + " base hire fee");
+    }
+    }
 
   public void createVenue(
     String venueName, String venueCode, String capacityInput, String hireFeeInput) {
@@ -51,9 +62,45 @@ public class VenueHireSystem {
     this.venueCode = venueCode;
     this.capacityInput = capacityInput;
     this.hireFeeInput = hireFeeInput;
+    venues.add(this);
+
     System.out.println("Successfully created venue '" + venueName + "' (" + venueCode + ").");
     }
 
+  public void countVenues(int venueCount) {
+    switch (venueCount) {
+      case 1: 
+        System.out.println("There is one venue in the system:");
+        break;
+      case 2:
+        System.out.println("There are two venues in the system:");
+        break;
+      case 3:
+        System.out.println("There are three venues in the system:");
+        break;
+      case 4:
+        System.out.println("There are four venues in the system:");
+        break;
+      case 5:
+        System.out.println("There are five venues in the system:");
+        break;
+      case 6:
+        System.out.println("There are six venues in the system:");
+        break;
+      case 7:
+        System.out.println("There are seven venues in the system:");
+        break;
+      case 8:
+        System.out.println("There are eight venues in the system:");
+        break;
+      case 9:
+        System.out.println("There are nine venues in the system:");
+        break;
+      default:
+        System.out.println("There are " + venueCount + " venues in the system:");
+        }
+  }
+    
   public void setSystemDate(String dateInput) {
     // TODO implement this method
   }
