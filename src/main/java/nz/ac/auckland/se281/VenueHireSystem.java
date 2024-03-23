38ba36982b97f2e17e4f7e45eeb5b637d27c720b
Kyle <klim385@aucklanduni.ac.nz>
2024-03-23 21:01:17 +1300

    Fixed createVenue method so it takes in proper inputs and ignores inputs with no venueName

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -4,6 +4,7 @@ import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
+  
   private String venueName;
   private String venueCode;
   private String capacityInput;
@@ -17,6 +18,11 @@ public class VenueHireSystem {
 
   public void createVenue(
     String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+    if (venueName == null || venueName.isEmpty())
+    {
+      System.out.println(MessageCli.VENUE_NOT_CREATED_EMPTY_NAME);
+      return;
+    }
     this.venueName = venueName;
     this.venueCode = venueCode;
     this.capacityInput = capacityInput;
