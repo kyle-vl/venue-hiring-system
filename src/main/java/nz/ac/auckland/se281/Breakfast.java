e59df9c3f0af67bd5bce8ac86ee952319ee6e080
Kyle <klim385@aucklanduni.ac.nz>
2024-04-08 15:01:49 +1200

    Added class Breakfast which extends Catering

---
+++ src/main/java/nz/ac/auckland/se281/Breakfast.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se281;
+
+public class Breakfast extends Catering {
+
+    public Breakfast(String bookingReference) {
+        super(bookingReference, "Breakfast", 15);
+    }
+
+    public void displayMessage(String bookingReference) {
+    MessageCli.ADD_SERVICE_SUCCESSFUL.printMessage("Catering (Breakfast)", bookingReference);
+    }
+}
